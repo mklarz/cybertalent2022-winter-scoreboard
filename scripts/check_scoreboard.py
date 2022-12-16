74b4c495de0ae62189bb98f7e12842d5868d8f16
mklarz <mklarz@protonmail.com>
2022-12-16 14:54:13 +0100

    quickfix

--- scripts/check_scoreboard.py
+++ scripts/check_scoreboard.py
@@ -7,6 +7,7 @@ import requests
 from urllib3.util.retry import Retry
 from requests.adapters import HTTPAdapter
 from random_user_agent.user_agent import UserAgent
+from random_user_agent.params import SoftwareName, OperatingSystem
 from bs4 import BeautifulSoup
 
 # dirty and quick script for tracking the highscore. buy me a beer :))
@@ -30,6 +31,11 @@ retries = Retry(
 )
 s = requests.Session()
 s.mount(HIGHSCORE_URL, HTTPAdapter(max_retries=retries))
+
+software_names = [SoftwareName.CHROME.value]
+operating_systems = [OperatingSystem.WINDOWS.value, OperatingSystem.LINUX.value]   
+user_agent_rotator = UserAgent(software_names=software_names, operating_systems=operating_systems, limit=100)
+
 r = s.get(HIGHSCORE_URL, headers={"User-Agent": user_agent_rotator.get_random_user_agent()})
 
 soup = BeautifulSoup(r.content, 'html.parser')
