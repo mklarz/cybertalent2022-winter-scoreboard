77a2154d12f275298df134079474a085cceab93f
mklarz <mklarz@protonmail.com>
2023-01-18 22:18:49 +0100

    correct path to star bug for users

--- scripts/generate_html.py
+++ scripts/generate_html.py
@@ -21,6 +21,7 @@ HIGHSCORE_HTML_PATH = BASE_PATH + "/index.html"
 HIGHSCORE_LIST_ITEM_HTML_FORMAT = '<li class=counter onclick="location=\'./users/{}.html\'"><span class="navn">{}</span><span class="sum">{}</span><span class="stars">{}</span></li>\n'
 HIGHSCORE_LIST_ITEM_UNKNOWN_HTML_FORMAT = '<li class=unknown onclick="location=\'./users/{}.html\'"><span class="navn">{}</span><span class="sum">{}</span><span class="stars">{}</span></li>\n'
 STAR_IMAGE_HTML = '<img src="./static/bug.gif" height="40px" alt="Stjerne: umulig"/>'
+STAR_USER_IMAGE_HTML = '<img src="../static/bug.gif" height="40px" alt="Stjerne: umulig"/>'
 
 USERS_PATH = BASE_PATH + "/users/"
 USER_JSON_PATH = USERS_PATH + "{}.json"
@@ -55,7 +56,7 @@ for user_id in USERS:
     user_html = user_html.replace("{USER_NAME}", bleach.clean(user_data["name"]))
     user_html = user_html.replace("{USER_POINTS}", str(user_data["points"]))
     user_html = user_html.replace("{USER_POSITION}", position_str)
-    user_html = user_html.replace("{USER_STARS}", user_data["stars"] * STAR_IMAGE_HTML)
+    user_html = user_html.replace("{USER_STARS}", user_data["stars"] * STAR_USER_IMAGE_HTML)
     categories_html = ""
     for category_name, percent in user_data["categories"].items():
         categories_html += USER_CATEGORY_HTML_FORMAT.format(category_name, percent)
